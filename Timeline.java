c765a645d7fa5b96a2bf68fd9f9dd70f59d126bb
Tony <awalker780@gmail.com>
2022-10-31 21:44:23 -0400

    comment practice in java

---
+++ Timeline.java
@@ -0,0 +1,22 @@
+/* https://www.codecademy.com/courses/learn-java/lessons/hello-world-java/exercises/commenting-code
+ 
+Practices using comments, same asa JS except for / * * which is used for API documentation-type comments
+*/
+
+public class Timeline {
+  public static void main(String[] args) {
+    System.out.println("Hello Java!");
+    
+    System.out.println("You were born in 1995");
+
+// Sun Microsystems announced the release of Java in 1995
+    
+    System.out.println("You were created by James Gosling");
+    
+/*		James Gosling is a Canadian engineer who 
+		created Java while working at Sun Microsystems. 
+		His favorite number is the square root of 2!
+*/
+    System.out.println("You are a fun language!");
+  }
+} 
