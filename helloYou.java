a19dcfe0f27e93ea86777ef505043cc40d1739aa
Tony <awalker780@gmail.com>
2022-10-31 21:35:05 -0400

    creates file to practice print statement

---
+++ helloYou.java
@@ -0,0 +1,7 @@
+// https://www.codecademy.com/courses/learn-java/lessons/hello-world-java/exercises/hello-java-file
+
+public class HelloYou {
+  public static void main(String[] args) {
+    System.out.println("Hello Tony!");
+  }
+}
