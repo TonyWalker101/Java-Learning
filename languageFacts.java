dd7939fe0619aaafde162bbb55d0c814e02afa2c
Tony <awalker780@gmail.com>
2022-10-31 21:52:56 -0400

    adds whitespaces and extra printed lines

--- languageFacts.java
+++ languageFacts.java
@@ -3,3 +3,18 @@ https://www.codecademy.com/courses/learn-java/lessons/hello-world-java/exercises
 
 Lesson on how whitespace is interpreted by Java/programmers
 */ 
+
+public class LanguageFacts {
+  public static void main(String[] args) {
+    // Press enter or return on your keyboard after each semicolon!
+    
+    System.out.println("Java is a class-based language.");
+
+    System.out.println("Java classes have a 'main' method.");
+    
+    System.out.println("Java statements end with a semicolon.");
+
+    System.out.println("Programming is... challenging..but fun!");
+  
+  }
+}
