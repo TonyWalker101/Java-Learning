4122756c87e9319acb749c4a791997823fe36661
Tony <awalker780@gmail.com>
2022-10-31 21:39:39 -0400

    practices print statements

---
+++ hideAndSeek.java
@@ -0,0 +1,18 @@
+/*
+https://www.codecademy.com/courses/learn-java/lessons/hello-world-java/exercises/print-statements
+
+Practices difference between System.out.print() and 
+System.out.println()
+
+*/ 
+
+public class HideAndSeek {
+  public static void main(String[] args) {
+    System.out.println("Let's play hide and seek.");
+    System.out.print("Three...");
+    System.out.print("Two...");
+    System.out.println("One...");
+    System.out.println("Ready or not, here I come!");
+
+  }
+}
