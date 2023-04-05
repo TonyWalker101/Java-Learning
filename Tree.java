d17c2318726fd950d12e27c81223b372a5569f0f
Tony <awalker780@gmail.com>
2023-04-05 17:27:38 -0400

    creates program logic

---
+++ Tree.java
@@ -0,0 +1,15 @@
+// practice file for learning basic Java print concepts
+
+public class Tree {
+  public static void main(String[] args) {
+    /* We're going to introduce ourselves and print a tree! */
+    System.out.println("Hey there, I'm Tony and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+        System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println("***** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+
+  }
+}
