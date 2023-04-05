5ffc09831b2180c75d5e0f0d537785b18b61537f
Tony <awalker780@gmail.com>
2023-04-05 18:15:16 -0400

    finalizes program

--- madlibs.java
+++ madlibs.java
@@ -1,14 +1,28 @@
 // program to practice creating variables
 
 public class MadLibs {
-  /*
-  Your description here
-  */
+
   	public static void main(String[] args){
-      
-      
+
+      //variables for story
+      String name1 = "Tony";
+      String adjective1 = "cold";
+      String adjective2 = "large";
+      String adjective3 = "funny";
+      String verb1 = "run";
+      String noun1 = "cat";
+      String noun2 = "apple";
+      String noun3 = "tree";
+      String noun4 = "train";
+      String noun5 = "backpack";
+      String noun6 = "shoe";
+      String name2 = "Katie";
+      int number = 7;
+      String place1 = "France";
       
       //The template for the story
       String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+      System.out.println(story);
     }       
 }
